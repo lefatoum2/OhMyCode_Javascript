5d71b5002f00d77d55081c2b1685bec128472aae
lefatoum2 <lefatoum@hotmail.fr>
2024-04-07 13:52:58 +0200

    Initial commit

---
+++ modele/script.js
@@ -0,0 +1,9 @@
+console.log('Connecté');
+//document.getElementById('titre').style.color = "green";
+const titre = document.getElementById('titre');
+console.log(titre);
+let score = 0;
+score = score +1;
+console.log(score);
+
+
